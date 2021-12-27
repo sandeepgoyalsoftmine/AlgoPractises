535c84317ac8985c93080b09b0b12fcd12e250a9
sandeep goyal <sandeep.goyal@softminesol.com>
2021-12-27 17:14:47 +0530

    Update Logic in linear order and return result in-place of size

--- src/com/company/array/Elements_which_have_atleast_two_greater_elements.java
+++ src/com/company/array/Elements_which_have_atleast_two_greater_elements.java
@@ -1,16 +1,39 @@
 package com.company.array;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Elements_which_have_atleast_two_greater_elements {
-    public int Solution(ArrayList<Integer> array) {
-        Collections.sort(array);
-        return array.size()-2;
+    public ArrayList<Integer> Solution(ArrayList<Integer> array) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if(array.size()<=2) {
+            return result;
+        }
+        int secondLargest = array.get(0)<array.get(1)?array.get(0):array.get(1);
+        int largest =array.get(0)<array.get(1)?array.get(1):array.get(0);
+        for (int numberr = 2;numberr<array.size();numberr++) {
+            int number = array.get(numberr);
+            if (number > largest) {
+                result.add(secondLargest);
+                secondLargest = largest;
+                largest = number;
+            } else if (number > secondLargest) {
+                result.add(secondLargest);
+                secondLargest = number;
+
+            } else {
+                result.add(number);
+            }
+
+
+        }
+        return result;
 
     }
+
     public static void main(String[] args) {
-        System.out.println(new Elements_which_have_atleast_two_greater_elements().Solution(new ArrayList(List.of(new Integer[]{11, 17, 100, 5}))));
+        System.out.println(new Elements_which_have_atleast_two_greater_elements().Solution(new ArrayList(List.of(new Integer[]{11, 17, 100, 5}))).toString());
     }
 }

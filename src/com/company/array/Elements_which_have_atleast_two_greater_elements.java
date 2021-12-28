97f142d1f060bf66e3731e21dc11d5615b316272
sandeep goyal <sandeep.goyal@softminesol.com>
2021-12-28 23:23:50 +0530

    Elements greater two numbers preserve sequence

--- src/com/company/array/Elements_which_have_atleast_two_greater_elements.java
+++ src/com/company/array/Elements_which_have_atleast_two_greater_elements.java
@@ -11,29 +11,37 @@ public class Elements_which_have_atleast_two_greater_elements {
         if(array.size()<=2) {
             return result;
         }
+
+
         int secondLargest = array.get(0)<array.get(1)?array.get(0):array.get(1);
-        int largest =array.get(0)<array.get(1)?array.get(1):array.get(0);
+        int largest = array.get(0)<array.get(1)?array.get(1):array.get(0);
+
         for (int numberr = 2;numberr<array.size();numberr++) {
             int number = array.get(numberr);
             if (number > largest) {
-                result.add(secondLargest);
                 secondLargest = largest;
                 largest = number;
             } else if (number > secondLargest) {
-                result.add(secondLargest);
                 secondLargest = number;
 
-            } else {
+            }
+
+        }
+        for (int numberr = 0;numberr<array.size();numberr++) {
+            int number = array.get(numberr);
+            if (number == largest || number == secondLargest) {
+                continue;
+            } else  {
                 result.add(number);
             }
 
-
         }
+
         return result;
 
     }
 
     public static void main(String[] args) {
-        System.out.println(new Elements_which_have_atleast_two_greater_elements().Solution(new ArrayList(List.of(new Integer[]{11, 17, 100, 5}))).toString());
+        System.out.println(new Elements_which_have_atleast_two_greater_elements().Solution(new ArrayList(List.of(new Integer[]{ 391, 634, 740, 441, 75, 444, 65, 611, 679, 59, 878, 102, 42, 190, 801, 571, 79, 686, 523, 580, 199, 497, 879, 334, 200, 202, 991, 341, 479, 563, 112, 550, 494, 468, 56, 644, 53, 581, 836, 461, 905, 849, 838, 434, 818, 350, 585, 280, 252, 834, 510, 420, 395, 776, 118, 886, 19, 809, 534, 143, 933, 15, 999, 514, 230, 531, 666, 841, 861, 703, 972, 622, 640, 21, 811, 476, 751, 430, 308, 996, 165, 812, 424, 412, 903, 601, 226, 239, 728, 135}))).toString());
     }
 }
